55e55e42ba80e03b798927eb1620604bdd084d18
MarineSutarik <marine.sutarik@yahoo.fr>
2018-04-21 13:07:39 +0200

    changement du package
    #5  Pour mieux correspondre au cours

--- src/main/java/domain/Enseignant.java
+++ src/main/java/domain/Enseignant.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modele;
+package domain;
 
 /**
  *
  * @author allan
  */
-public class President extends Membre{
+public class Enseignant extends Membre{
     
 }
