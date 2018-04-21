5572f798f7bdb55ba2d36d1b4c64f37a54dcd996
MarineSutarik <marine.sutarik@yahoo.fr>
2018-04-21 14:01:31 +0200

    #3
    Je vais manger :)

--- src/main/java/services/GestionMembre.java
+++ src/main/java/services/GestionMembre.java
@@ -5,10 +5,30 @@
  */
 package services;
 
+
+import java.util.Date;
+
 /**
  *
  * @author squatteur
  */
 public interface GestionMembre {
-    
+
+    /**
+     *
+     * @param idMembre
+     * @param nom
+     * @param prenom
+     * @param adresseMail
+     * @param login
+     * @param dateDebutCertificat
+     * @param asPaye
+     * @param estApte
+     * @param niveauExpertise
+     * @param numLicence
+     * @param idAdresse
+     * @param pays
+     * @param ville
+     */
+    public void creerMembre (Integer idMembre, String nom, String prenom, String adresseMail, String login, Date dateDebutCertificat, boolean asPaye, boolean estApte, Integer niveauExpertise, String numLicence,Integer idAdresse, String pays, String ville);
 }
--- src/main/java/services/GestionMembreImp.java
+++ src/main/java/services/GestionMembreImp.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package services;
+
+import domain.Membre;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 /**
  *
- * @author squatteur
+ * @author Marine
  */
+@Service
 public class GestionMembreImp implements GestionMembre{
+
+    @Autowired
+    private Membre membre;
+    
+    @Override
+    public void creerMembre(Integer idMembre, String nom, String prenom, String adresseMail, String login, Date dateDebutCertificat, boolean asPaye, boolean estApte, Integer niveauExpertise, String numLicence, Integer idAdresse, String pays, String ville) {
+        
+    }
     
 }
