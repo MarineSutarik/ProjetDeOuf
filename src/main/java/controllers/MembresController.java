9eb7536550bb5734595be9d75f607357690ba760
Vivien Tournié <vivien.tournie46@gmail.com>
2018-05-13 17:39:43 +0200

    Refactor Package "controllers"

--- src/main/java/controllers/MembresController.java
+++ src/main/java/controllers/MembresController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controllers;
+package controllers;
 
 import domain.*; 
 import org.springframework.web.bind.annotation.RequestParam;
